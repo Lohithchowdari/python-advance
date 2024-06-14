be996756d0e462fc9b5e8283cc790bf5857726ad
DUPATI LOHITH CHOWDARI <113517733+Lohithchowdari@users.noreply.github.com>
2024-06-14 20:49:22 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,28 @@
+##FOR slicing
+"""
+str = "LOHITH CHOWDARI"
+print(str[-3:-1])
+"""
+#for input name and print length
+"""
+name = input("your nam : ")
+print(len(name))
+"""
+#if else conditions
+"""
+number = int( input ( "Enter the number : "))
+if(number % 2 == 0 ):
+    print("The number is even")
+else:
+    print("It's odd")
+"""
+#if else conditions
+a = int(input("Enter the number : "))
+b = int(input("Enter the number : "))
+c = int(input("Enter the number : "))
+if(a>=b and a>=c) :
+    print("A is the grater number")
+elif( b>=c) :
+    print("B is the grater number")
+else:
+    print("C is the grater number")
