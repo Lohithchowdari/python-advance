ddaa65123b7779dadf1412562e4cedd52fece54e
DUPATI LOHITH CHOWDARI <113517733+Lohithchowdari@users.noreply.github.com>
2024-07-26 10:00:39 +0530

    Add files via upload

---
+++ learing.py
@@ -0,0 +1,47 @@
+
+#lecter 3
+"""
+a=str(input("Enter the fourt movie name : "))
+b=str(input("Enter the fourt movie name : "))
+c=str(input("Enter the fourt movie name : "))
+list=[a,b,c]
+print(list)
+"""
+"""
+list = []
+list.append(input("Enter : "))
+list.append(input("Enter : "))
+list.append(input("Enter : "))
+list.append(input("Enter : "))
+list.append(input("Enter : "))
+print(list.count("A"))
+"""
+"""
+list2 = ["A","B","C","B","D","P","M"]
+list2.sort()
+print(list2)
+"""
+#Lecture 4 : Dictionary & Set in Python
+"""
+dictionary  = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
+}
+print(dictionary)
+"""
+"""
+subjects = {
+    "python" ,"java","C++","python","javascript","java","python","java","C++","C"
+}
+print (len(subjects))
+"""
+"""
+marks = {}
+x = int(input("Enter maths marks : "))
+marks.update({"maths" : x} )
+y = int(input("Enter phy marks : "))
+marks.update({"phy" : y })
+z = int(input("Enter chem marks : "))
+marks.update({"chem" : z })
+print(marks)
+"""
